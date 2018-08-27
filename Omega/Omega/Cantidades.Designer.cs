5ef3e0b4b96f65090d62b5ee2107a73e5741c120
Cami <camilaa@lagash.com>
2018-08-27 20:33:49 -0300

    Agregado de icono y centrado

--- Omega/Omega/Cantidades.Designer.cs
+++ Omega/Omega/Cantidades.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Cantidades));
             this.btnAceptar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.txtRespuesta = new System.Windows.Forms.TextBox();
@@ -79,7 +80,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtRespuesta);
             this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Cantidades";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cantidades";
             this.Load += new System.EventHandler(this.Cantidades_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
