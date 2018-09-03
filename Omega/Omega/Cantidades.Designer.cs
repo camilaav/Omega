22e9d7290e0c051b8bc19454bdf9dc8bdf5b4ab3
DESKTOP-5D79K30\Nehuén <nehuenlerose21@gmail.com>
2018-09-03 19:00:32 -0300

    no message

--- Omega/Omega/Cantidades.Designer.cs
+++ Omega/Omega/Cantidades.Designer.cs
@@ -28,38 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnAceptar = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtRespuesta = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // btnAceptar
-            // 
-            this.btnAceptar.Location = new System.Drawing.Point(186, 427);
-            this.btnAceptar.Name = "btnAceptar";
-            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
-            this.btnAceptar.TabIndex = 7;
-            this.btnAceptar.Text = "Aceptar";
-            this.btnAceptar.UseVisualStyleBackColor = true;
-            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(183, 385);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 13);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Opción correcta";
-            // 
-            // txtRespuesta
-            // 
-            this.txtRespuesta.Location = new System.Drawing.Point(176, 401);
-            this.txtRespuesta.Name = "txtRespuesta";
-            this.txtRespuesta.Size = new System.Drawing.Size(100, 20);
-            this.txtRespuesta.TabIndex = 5;
             // 
             // pictureBox1
             // 
@@ -75,24 +46,16 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(501, 461);
-            this.Controls.Add(this.btnAceptar);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtRespuesta);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Cantidades";
             this.Text = "Cantidades";
             this.Load += new System.EventHandler(this.Cantidades_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnAceptar;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtRespuesta;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
