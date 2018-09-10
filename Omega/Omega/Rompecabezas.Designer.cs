fa1a87382ec14b0115d6b67c309261e796a82ffc
LAGASHSYSTEMS\camilaa <camilaa@lagash.com>
2018-09-09 23:58:27 -0300

    comienzo de rompecabezas

---
+++ Omega/Omega/Rompecabezas.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Omega
+{
+    partial class Rompecabezas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Rompecabezas));
+            this.Elegir = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.imagen = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Elegir
+            // 
+            this.Elegir.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Elegir.Location = new System.Drawing.Point(567, 324);
+            this.Elegir.Name = "Elegir";
+            this.Elegir.Size = new System.Drawing.Size(75, 48);
+            this.Elegir.TabIndex = 7;
+            this.Elegir.Text = "Elegir imagen";
+            this.Elegir.UseVisualStyleBackColor = true;
+            this.Elegir.Click += new System.EventHandler(this.Elegir_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(150, 150);
+            this.panel1.TabIndex = 8;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Location = new System.Drawing.Point(168, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(150, 150);
+            this.panel2.TabIndex = 9;
+            this.panel2.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel2.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.pictureBox3);
+            this.panel3.Location = new System.Drawing.Point(12, 168);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(150, 150);
+            this.panel3.TabIndex = 9;
+            this.panel3.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel3.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.pictureBox4);
+            this.panel4.Location = new System.Drawing.Point(168, 168);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(150, 150);
+            this.panel4.TabIndex = 9;
+            this.panel4.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel4.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "0";
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "0";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "0";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 13;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "0";
+            // 
+            // imagen
+            // 
+            this.imagen.Location = new System.Drawing.Point(346, 12);
+            this.imagen.Name = "imagen";
+            this.imagen.Size = new System.Drawing.Size(296, 306);
+            this.imagen.TabIndex = 12;
+            this.imagen.TabStop = false;
+            // 
+            // Rompecabezas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 422);
+            this.Controls.Add(this.imagen);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Elegir);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Rompecabezas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rompecabezas";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagen)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Elegir;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox imagen;
+    }
+}
---
+++ Omega/Omega/Rompecabezas.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Omega
+{
+    public partial class Rompecabezas : Form
+    {
+        string foto;
+        Panel [] panel = new Panel [4];
+        PictureBox[] pictureBox = new PictureBox[4];
+        int np;
+
+        public void Romper()
+        {
+            Bitmap bitO, bit;
+            int anc, lar, x, y, i;
+            Rectangle area;
+            bitO = new Bitmap(foto);
+            anc = bitO.Width / 2;
+            lar = bitO.Height / 2;
+            i = 0;
+            x = 0;
+            y = 0;
+            do
+            {
+                area = new Rectangle(x, y, anc, lar);
+                bit = new Bitmap(anc, lar);
+                bit = bitO.Clone(area, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                pictureBox[i].Image = bit;
+                if ((i + 1) % 2 == 0)
+                {
+                    y = y + lar;
+                    x = 0;
+                }
+                else
+                {
+                    x = x + anc;
+                }
+                i++;
+            } while (i<4);
+            Revolver();
+        }
+
+        public Image Gira(Image ima)
+        {
+            Bitmap bit;
+            bit = new Bitmap(ima);
+            bit.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            return bit;
+        }
+
+        public void Revolver()
+        {
+            Random random = new Random();
+            int i, a, v, j;
+            int[] n = new int[4];
+            n[0] = 0;
+            n[1] = 1;
+            n[2] = 2;
+            n[3] = 3;
+            for(i = 0; i<3;i++)
+            {
+                a = random.Next(i + 1,3);
+                v = n[1];
+                n[i] = n[a];
+                n[a] = v;
+            }
+            for(i=0;i<=3;i++)
+            {
+                pictureBox[i].Parent = panel[n[i]];
+                a = random.Next(1, 4);
+                for(j=1;j<=a;j++)
+                {
+                    pictureBox[i].Image = Gira(pictureBox[i].Image);
+                }
+            }
+        }
+
+        public Rompecabezas()
+        {
+            InitializeComponent();
+            panel[0] = panel1;
+            panel[1] = panel2;
+            panel[2] = panel3;
+            panel[3] = panel4;
+            pictureBox[0] = pictureBox1;
+            pictureBox[1] = pictureBox2;
+            pictureBox[2] = pictureBox3;
+            pictureBox[3] = pictureBox4;
+        }
+
+        private void Elegir_Click(object sender, EventArgs e)
+        {
+            foto = string.Empty;
+            while(foto == string.Empty)
+            {
+                openFileDialog.ShowDialog();
+                foto = openFileDialog.FileName;
+            }
+            imagen.Load(foto);
+            Romper();
+        }
+
+        private void pictureBox1_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            PictureBox picture = sender as PictureBox;
+            np = Convert.ToInt16(picture.Tag.ToString());
+            if (e.Button == MouseButtons.Left)
+            {
+                DoDragDrop(pictureBox[np], DragDropEffects.Move);
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                pictureBox[np].Image = Gira(pictureBox[np].Image);
+            }
+        }
+
+        private void panel1_DragEnter(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetDataPresent(typeof(PictureBox)))
+            {
+                e.Effect = DragDropEffects.Move;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            Panel pa = sender as Panel;
+            if(e.Data.GetDataPresent(typeof(PictureBox)))
+            {
+                pictureBox[np].Parent = pa;
+                pictureBox[np].BringToFront();
+            }
+        }
+    }
+}
