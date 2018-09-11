a2654e2a2c280ee4fb373738dc396cca3d7bda98
Mariano <diaz.marianonicolas@gmail.com>
2018-09-10 21:03:05 -0300

    CAmbio de imagenes a labels

--- Omega/Omega/Resta.Designer.cs
+++ Omega/Omega/Resta.Designer.cs
@@ -30,126 +30,16 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Resta));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.pictureBox6 = new System.Windows.Forms.PictureBox();
-            this.Rta1 = new System.Windows.Forms.PictureBox();
-            this.Rta2 = new System.Windows.Forms.PictureBox();
-            this.Rta3 = new System.Windows.Forms.PictureBox();
             this.tiempo = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.Rta1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.Rta2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.Rta3)).BeginInit();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Location = new System.Drawing.Point(34, 177);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(106, 154);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 6;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox2.Location = new System.Drawing.Point(243, 177);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(106, 154);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 7;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox3.Location = new System.Drawing.Point(442, 177);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(106, 154);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 8;
-            this.pictureBox3.TabStop = false;
-            // 
-            // pictureBox4
-            // 
-            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox4.Location = new System.Drawing.Point(156, 243);
-            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(71, 29);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox4.TabIndex = 9;
-            this.pictureBox4.TabStop = false;
-            // 
-            // pictureBox5
-            // 
-            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox5.Location = new System.Drawing.Point(364, 236);
-            this.pictureBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(64, 29);
-            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox5.TabIndex = 10;
-            this.pictureBox5.TabStop = false;
-            // 
-            // pictureBox6
-            // 
-            this.pictureBox6.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox6.Location = new System.Drawing.Point(364, 271);
-            this.pictureBox6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(64, 29);
-            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox6.TabIndex = 11;
-            this.pictureBox6.TabStop = false;
-            // 
-            // Rta1
-            // 
-            this.Rta1.BackColor = System.Drawing.Color.Transparent;
-            this.Rta1.Location = new System.Drawing.Point(869, 63);
-            this.Rta1.Name = "Rta1";
-            this.Rta1.Size = new System.Drawing.Size(100, 100);
-            this.Rta1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.Rta1.TabIndex = 14;
-            this.Rta1.TabStop = false;
-            this.Rta1.Click += new System.EventHandler(this.pictureBox7_Click);
-            // 
-            // Rta2
-            // 
-            this.Rta2.BackColor = System.Drawing.Color.Transparent;
-            this.Rta2.Location = new System.Drawing.Point(869, 200);
-            this.Rta2.Name = "Rta2";
-            this.Rta2.Size = new System.Drawing.Size(100, 100);
-            this.Rta2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.Rta2.TabIndex = 15;
-            this.Rta2.TabStop = false;
-            this.Rta2.Click += new System.EventHandler(this.Rta2_Click);
-            // 
-            // Rta3
-            // 
-            this.Rta3.BackColor = System.Drawing.Color.Transparent;
-            this.Rta3.Location = new System.Drawing.Point(869, 340);
-            this.Rta3.Name = "Rta3";
-            this.Rta3.Size = new System.Drawing.Size(100, 100);
-            this.Rta3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.Rta3.TabIndex = 16;
-            this.Rta3.TabStop = false;
-            this.Rta3.Click += new System.EventHandler(this.Rta3_Click);
             // 
             // tiempo
             // 
@@ -159,62 +49,142 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("K26ToyBlocks123", 200.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 108);
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Fuchsia;
+            this.label1.Location = new System.Drawing.Point(96, 199);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(591, 278);
-            this.label1.TabIndex = 17;
-            this.label1.Text = "12";
+            this.label1.Size = new System.Drawing.Size(184, 149);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "10";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Showcard Gothic", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label2.Location = new System.Drawing.Point(253, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 149);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "-";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Showcard Gothic", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.Location = new System.Drawing.Point(344, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(184, 149);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "10";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F);
+            this.label4.Location = new System.Drawing.Point(590, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 135);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "=";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 80F);
+            this.label5.Location = new System.Drawing.Point(712, 195);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(290, 153);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "100";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 80F);
+            this.label6.Location = new System.Drawing.Point(1101, 58);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(290, 153);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "100";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 80F);
+            this.label7.Location = new System.Drawing.Point(1101, 218);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(290, 153);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "100";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 80F);
+            this.label8.Location = new System.Drawing.Point(1101, 388);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(290, 153);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "100";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label8.Click += new System.EventHandler(this.label8_Click);
             // 
             // Resta
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Omega.Properties.Resources.FondoEscuela;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1028, 516);
+            this.ClientSize = new System.Drawing.Size(1371, 635);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.Rta3);
-            this.Controls.Add(this.Rta2);
-            this.Controls.Add(this.Rta1);
-            this.Controls.Add(this.pictureBox6);
-            this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Resta";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Resta";
             this.Load += new System.EventHandler(this.Resta_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.Rta1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.Rta2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.Rta3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.PictureBox pictureBox5;
-        private System.Windows.Forms.PictureBox pictureBox6;
-        private System.Windows.Forms.PictureBox Rta1;
-        private System.Windows.Forms.PictureBox Rta2;
-        private System.Windows.Forms.PictureBox Rta3;
         private System.Windows.Forms.Timer tiempo;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
--- Omega/Omega/Resta.cs
+++ Omega/Omega/Resta.cs
@@ -29,18 +29,24 @@ namespace Omega
         int contF;
         int contM;
         int contD;
+        private void Limpiar()
+        {
+            label1.Text = null;
+            label3.Text = null;
+            label5.Text = null;
+            label6.Text = null;
+            label7.Text = null;
+            label8.Text = null;
+        }
 
         private void Resta_Load(object sender, EventArgs e)
         {
-            tiempo.Enabled = true;
+            
+            Limpiar();
+            label5.Visible = false;
 
-            pictureBox3.Visible = false;
-
-            pictureBox4.Image = Resources.Menos;
-            pictureBox5.Image = Resources.Menos;
-            pictureBox6.Image = Resources.Menos;
-
-
+            //JuegoFacil();
+            JuegoDificil();
             //if (Dificultado == 1)
             //{
             //    if (contF < 5)
@@ -63,92 +69,54 @@ namespace Omega
             //        JuegoDificil();
             //    }
             //}
-
+            label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 
             if (orden == 0)
             {
-                Rta1.Image = Image.FromFile(Path.Combine(Application.StartupPath, "img\\" + resultado.ToString() + ".png"));
-                Rta2.Image = Image.FromFile(Path.Combine(Application.StartupPath, "img\\" + fondo.ToString() + ".png"));
-                Rta3.Image = Image.FromFile(Path.Combine(Application.StartupPath, "img\\" + fondo2.ToString() + ".png"));
+                label6.Text = resultado.ToString();
+                label7.Text = fondo.ToString();
+                label8.Text = fondo2.ToString();
             }
             if (orden == 1)
             {
-                Rta1.Image = Image.FromFile(Path.Combine(Application.StartupPath, "img\\" + fondo.ToString() + ".png"));
-                Rta2.Image = Image.FromFile(Path.Combine(Application.StartupPath, "img\\" + resultado.ToString() + ".png"));
-                Rta3.Image = Image.FromFile(Path.Combine(Application.StartupPath, "img\\" + fondo2.ToString() + ".png"));
+                label6.Text = fondo.ToString();
+                label7.Text = resultado.ToString();
+                label8.Text = fondo2.ToString();
             }
             if (orden == 2)
             {
-                Rta1.Image = Image.FromFile(Path.Combine(Application.StartupPath, "img\\" + fondo2.ToString() + ".png"));
-                Rta2.Image = Image.FromFile(Path.Combine(Application.StartupPath, "img\\" + fondo.ToString() + ".png"));
-                Rta3.Image = Image.FromFile(Path.Combine(Application.StartupPath, "img\\" + resultado.ToString() + ".png"));
+                label6.Text = fondo2.ToString();
+                label7.Text = fondo.ToString();
+                label8.Text = resultado.ToString();
             }
         }
 
-      
+
 
         private void pictureBox7_Click(object sender, EventArgs e)
         {
             if (orden == 0)
             {
-                pictureBox3.Image = null;
+
                 MessageBox.Show("Muy bien!");
-                pictureBox3.Visible = true;
-                pictureBox3.Image = Image.FromFile(Path.Combine(Application.StartupPath, "img\\" + resultado.ToString() + ".png"));
+                label5.Visible = true;
+                label5.Text = resultado.ToString();
             }
             else
             {
-                pictureBox3.Image = null;
                 //MessageBox.Show("Respuesta incorrecta");                
-                pictureBox3.Image = Image.FromFile(Path.Combine(Application.StartupPath, "img\\cancel.png"));
-                pictureBox3.Visible = true;
-                tiempo.Enabled = true;
-                tiempo.Start();
-                if (tiempo.Interval > 10000)
-                {
-                    pictureBox3.Visible = false;
-                }
-            }
-        }
 
-        private void Rta2_Click(object sender, EventArgs e)
-        {
-            if (orden == 1)
-            {
-                pictureBox3.Image = null;
-                MessageBox.Show("Muy bien!");
-                pictureBox3.Visible = true;
-                pictureBox3.Image = Image.FromFile(Path.Combine(Application.StartupPath, "img\\" + resultado.ToString() + ".png"));
-            }
-            else
-            {
-                pictureBox3.Image = null;
-                //MessageBox.Show("Respuesta incorrecta");
-                pictureBox3.Image = Image.FromFile(Path.Combine(Application.StartupPath, "img\\cancel.png"));
-                pictureBox3.Visible = true;
+                label5.Text = "X";
+                label5.Visible = true;
             }
         }
-
-        private void Rta3_Click(object sender, EventArgs e)
-        {
-            if (orden == 2)
-            {
-                pictureBox3.Image = null;
-                MessageBox.Show("Muy bien!");
-                pictureBox3.Visible = true;
-                pictureBox3.Image = Image.FromFile(Path.Combine(Application.StartupPath, "img\\" + resultado.ToString() + ".png"));
-            }
-            else
-            {
-                pictureBox3.Image = null;
-                //MessageBox.Show("Respuesta incorrecta");
-                pictureBox3.Image = Image.FromFile(Path.Combine(Application.StartupPath, "img\\cancel.png"));
-                pictureBox3.Visible = true;
-                tiempo.Start();
-            }
-        }
+        
         private void JuegoFacil()
         {
+            label1.Width = 191;
+            label3.Width = 191;
+            contF++;
             numero1 = randommizer.Next(1, 10);
             numero2 = randommizer.Next(1, 10);
             resultado = (numero1 - numero2);
@@ -160,9 +128,9 @@ namespace Omega
                 resultado = (numero1 - numero2);
             }
             //LE DA IMAGENES A LOS 3 PRIMEROS PICTUREBOX
-            pictureBox1.Image = Image.FromFile(Path.Combine(Application.StartupPath, "img\\" + numero1.ToString() + ".png"));
-            pictureBox2.Image = Image.FromFile(Path.Combine(Application.StartupPath, "img\\" + numero2.ToString() + ".png"));
-            pictureBox3.Image = Image.FromFile(Path.Combine(Application.StartupPath, "img\\" + resultado.ToString() + ".png"));
+            label1.Text = numero1.ToString();
+            label3.Text = numero2.ToString();
+            label5.Text = resultado.ToString();
 
             orden = randommizer.Next(2);//NUMERO RANDOM PARA LA POSICION DE LA RESPUESTA CORRECTA
 
@@ -181,6 +149,8 @@ namespace Omega
         }
         private void JuegoMedio()
         {
+            label1.Width = 191;
+            label3.Width = 191;
             contM++;
             numero1 = randommizer.Next(0, 50);
             numero2 = randommizer.Next(0, 50);
@@ -193,10 +163,9 @@ namespace Omega
                 resultado = (numero1 - numero2);
             }
             //LE DA IMAGENES A LOS 3 PRIMEROS PICTUREBOX
-            pictureBox1.Image = Image.FromFile(Path.Combine(Application.StartupPath, "img\\" + numero1.ToString() + ".png"));
-            pictureBox2.Image = Image.FromFile(Path.Combine(Application.StartupPath, "img\\" + numero2.ToString() + ".png"));
-            pictureBox3.Image = Image.FromFile(Path.Combine(Application.StartupPath, "img\\" + resultado.ToString() + ".png"));
-
+            label1.Text = numero1.ToString();
+            label3.Text = numero2.ToString();
+            label5.Text = resultado.ToString();
             orden = randommizer.Next(2);//NUMERO RANDOM PARA LA POSICION DE LA RESPUESTA CORRECTA
 
             //NUMERO DE FONDO DE UN PICTURE DE RESPUESTAS
@@ -215,6 +184,8 @@ namespace Omega
         }
         private void JuegoDificil()
         {
+            label1.Width = 258;
+            label3.Width = 258;
             contD++;
             numero1 = randommizer.Next(0, 100);
             numero2 = randommizer.Next(0, 100);
@@ -227,10 +198,9 @@ namespace Omega
                 resultado = (numero1 - numero2);
             }
             //LE DA IMAGENES A LOS 3 PRIMEROS PICTUREBOX
-            pictureBox1.Image = Image.FromFile(Path.Combine(Application.StartupPath, "img\\" + numero1.ToString() + ".png"));
-            pictureBox2.Image = Image.FromFile(Path.Combine(Application.StartupPath, "img\\" + numero2.ToString() + ".png"));
-            pictureBox3.Image = Image.FromFile(Path.Combine(Application.StartupPath, "img\\" + resultado.ToString() + ".png"));
-
+            label1.Text = numero1.ToString();
+            label3.Text = numero2.ToString();
+            label5.Text = resultado.ToString();
             orden = randommizer.Next(2);//NUMERO RANDOM PARA LA POSICION DE LA RESPUESTA CORRECTA
 
             //NUMERO DE FONDO DE UN PICTURE DE RESPUESTAS
@@ -245,6 +215,65 @@ namespace Omega
                 fondo = randommizer.Next(0, 100);
                 fondo2 = randommizer.Next(0, 100);
             }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            if (orden == 0)
+            {
+
+                MessageBox.Show("Muy bien!");
+                label5.Visible = true;
+                label5.Text = resultado.ToString();
+            }
+            else
+            {
+                //MessageBox.Show("Respuesta incorrecta");                
+
+                label5.Text = "X";
+                label5.Visible = true;
+            }
+        }
+        private void label7_Click(object sender, EventArgs e)
+        {
+            if (orden == 1)
+            {
+
+                MessageBox.Show("Muy bien!");
+                label5.Visible = true;
+                label5.Text = resultado.ToString();
+            }
+            else
+            {
+                //MessageBox.Show("Respuesta incorrecta");                
+
+                label5.Text = "X";
+                label5.Visible = true;
+            }
+        }
+        private void label8_Click(object sender, EventArgs e)
+        {
+            if (orden == 2)
+            {
+                MessageBox.Show("Muy bien!");
+                label5.Visible = true;
+                label5.Text = resultado.ToString();
+            }
+            else
+            {
+                label5.Text = "X";
+                label5.Visible = true;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
