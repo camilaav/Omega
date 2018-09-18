1005b29875821e3c24010c0c27da01c73485a5ed
Mariano <diaz.marianonicolas@gmail.com>
2018-09-17 21:46:33 -0300

    Reorden de codigo

--- Omega/Omega/Program.cs
+++ Omega/Omega/Program.cs
@@ -27,7 +27,7 @@ namespace Omega
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Dificultades());
+            Application.Run(new Bienvenida());
         }
     }
 }
