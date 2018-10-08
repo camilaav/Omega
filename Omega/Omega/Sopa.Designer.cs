cbe4288f9f8e9a9a1143ceb1dc37193713176d34
Mariano <diaz.marianonicolas@gmail.com>
2018-10-08 18:52:29 -0300

    no message

--- Omega/Omega/Sopa.Designer.cs
+++ Omega/Omega/Sopa.Designer.cs
@@ -35,28 +35,41 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 15);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(415, 255);
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView1.Size = new System.Drawing.Size(752, 431);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(461, 42);
+            this.button1.Location = new System.Drawing.Point(849, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Sopa
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(643, 279);
+            this.ClientSize = new System.Drawing.Size(962, 459);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Sopa";
             this.Text = "Sopa";
             this.Load += new System.EventHandler(this.Sopa_Load);
--- Omega/Omega/Sopa.cs
+++ Omega/Omega/Sopa.cs
@@ -12,6 +12,7 @@ namespace Omega
 {
     public partial class Sopa : Form
     {
+        Random letras = new Random();
         public Sopa()
         {
             InitializeComponent();
@@ -32,13 +33,91 @@ namespace Omega
                 dataGridView1.RowCount = 6;
                 dataGridView1.ColumnCount = 6;
             }
-//             4x5 3 o 4 letras.
-//5x5 y las palabras a buscar serán de 4 o 5 letras.
-//6x6 y las palabras a buscar serán de 5 o 6 letras.
+//             4x4 y 5x5 3 o 4 letras.
+//6x6 7x7 y las palabras a buscar serán de 4 o 5 letras.
+//8x8 9x9 y las palabras a buscar serán de 5 o 6 letras.
+
+        }
+
+        private void Juego()
+        {
 
         }
 
         private void Sopa_Load(object sender, EventArgs e)
+        {
+            TablaFacil();
+            IniciarPalabras();
+        }
+
+        private void IniciarPalabras()
+        {
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    dataGridView1[j, i].Value = Convert.ToChar(letras.Next(65, 90));
+                    Facilcfg2();
+                }
+            }
+        }
+
+        private void Facilcfg1()
+        {
+            dataGridView1[1, 0].Value = 'C';
+            dataGridView1[1, 1].Value = 'A';
+            dataGridView1[1, 2].Value = 'S';
+            dataGridView1[1, 3].Value = 'A';
+
+            dataGridView1[0, 1].Value = 'G';
+            dataGridView1[1, 1].Value = 'A';
+            dataGridView1[2, 1].Value = 'T';
+            dataGridView1[3, 1].Value = 'O';
+            //raton
+            dataGridView1[3, 0].Value = 'R';
+            dataGridView1[3, 1].Value = 'A';
+            dataGridView1[3, 2].Value = 'T';
+            dataGridView1[3, 3].Value = 'O';
+            dataGridView1[3, 4].Value = 'N';
+        }
+
+        private void Facilcfg2()
+        {
+            dataGridView1[1, 2].Value = 'G';
+            dataGridView1[2, 2].Value = 'A';
+            dataGridView1[3, 2].Value = 'T';
+            dataGridView1[4, 2].Value = 'O';
+
+            dataGridView1[0, 1].Value = 'C';
+            dataGridView1[1, 1].Value = 'A';
+            dataGridView1[2, 1].Value = 'S';
+            dataGridView1[3, 1].Value = 'A';
+
+            //raton
+            dataGridView1[3, 0].Value = 'R';
+            dataGridView1[3, 1].Value = 'A';
+            dataGridView1[3, 2].Value = 'T';
+            dataGridView1[3, 3].Value = 'O';
+            dataGridView1[3, 4].Value = 'N';
+        }
+
+        private void Facilcfg3()
+        {
+
+        }
+
+        private void TablaFacil()
+        {
+            dataGridView1.RowCount = 5;
+            dataGridView1.ColumnCount = 5;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
