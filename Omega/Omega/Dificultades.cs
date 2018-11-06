9d71921e58057f4c7f7711d4a534722d9dbb07d8
Cami <camilaa@lagash.com>
2018-11-06 10:48:34 -0300

    Dificultades con imágenes correctas

--- Omega/Omega/Dificultades.cs
+++ Omega/Omega/Dificultades.cs
@@ -41,14 +41,6 @@ namespace Omega
             dictionary.Add("rompe", new MainForm());
             dictionary.Add("memotest", new Memotest());
 
-            string startupPath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "Omega", "Imágenes");
-            var listaDificultades = juegoRN.ListaDificultades();
-            //label1.Text = listaDificultades[0].NombreDificultad;
-          //  label2.Text = listaDificultades[1].NombreDificultad;
-         //   label3.Text = listaDificultades[2].NombreDificultad;
-            pictureBox1.BackgroundImage = Image.FromFile(startupPath + @"\" + listaDificultades[0].NombreDificultad+".png");
-            pictureBox2.BackgroundImage = Image.FromFile(startupPath + @"\" + listaDificultades[1].NombreDificultad + ".png");
-            pictureBox3.BackgroundImage = Image.FromFile(startupPath + @"\" + listaDificultades[2].NombreDificultad + ".png");
             pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
             pictureBox2.BackgroundImageLayout = ImageLayout.Stretch;
             pictureBox3.BackgroundImageLayout = ImageLayout.Stretch;
