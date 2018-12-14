d2c851dca845b7b423ee07850f074ad0ca3390ee
Cami <camilaa@lagash.com>
2018-12-13 23:29:41 -0300

    Cambio de framework a .net 4

---
+++ Omega/Entities/Movimiento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entidades
+{
+    public class Movimiento
+    {
+        public int IdMovimiento { get; set; }
+        public int Puntuacion { get; set; }
+        public DateTime Fecha { get; set; }
+        public static string Jugador { get; set; }
+        public int IdJuego { get; set; }
+        public int IdDificultad { get; set; }
+    }
+}
